f99563d38b48593f7968429285563b0a2c84a74d
insub <vb15@kakao.com>
2024-04-02 14:15:54 +0900

    initial

--- TaskCancelKitExample/ContentView.swift
+++ TaskCancelKitExample/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import TaskCancelKit
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Count: \(viewModel.count)")
+                .font(.title)
+                .padding(.bottom, 8)
+            Button("Start Counting") {
+                viewModel.startCountFive()
+            }
+            Button("Cancel Count") {
+                viewModel.cancelTask()
+            }
         }
         .padding()
+    }
+}
+
+class ViewModel: ObservableObject {
+    
+    @Published var count = 0
+    private let taskBag = TaskCancelBag<TaskID>()
+    
+    enum TaskID {
+        case countFive
+    }
+    
+    func startCountFive() {
+        Task { @MainActor in
+            for i in 0...5 {
+                count = i
+                try await Task.sleep(for: .seconds(1))
+            }
+        }.store(in: taskBag, id: .countFive)
+    }
+    
+    func cancelTask() {
+        taskBag.cancel(id: .countFive)
     }
 }
 
