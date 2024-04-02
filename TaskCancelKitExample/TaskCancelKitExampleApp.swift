4d7d3c009eeba17b82383d1fd07039a8391ae115
insub <vb15@kakao.com>
2024-04-02 14:04:51 +0900

    Initial Commit

---
+++ TaskCancelKitExample/TaskCancelKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCancelKitExampleApp.swift
+//  TaskCancelKitExample
+//
+//  Created by 김인섭 on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskCancelKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
